a3e119b3bceec51e5c89322f0053357623cbea35
Yinuo Peng <yinuop2@uci.edu>
2024-10-19 00:23:52 +0000

    complete print_menu.cpp

--- src/print_menu.cpp
+++ src/print_menu.cpp
@@ -1,1 +1,15 @@
 // complete the source code
+#include "../include/print_menu.h"
+#include <iostream>
+using namespace std;
+
+void print_menu(){
+    cout << "1: Enter 1 to double the number." << endl;
+    cout << "2: Enter 2 to reverse the digits of the number." << endl;
+    cout << "3: Enter 3 to raise the number to the power of 2, 3, or 4." << endl;
+    cout << "4: Enter 4 to sum the digits of the number." << endl;
+    cout << "5: Enter 5 to raise the first digit to the power of the second digit." << endl;
+    cout << "6: Enter 6 to raise the first two digits the power of the last digit." << endl;
+    cout << "9: Enter 9 to terminate the program." << endl;
+    return;
+}
