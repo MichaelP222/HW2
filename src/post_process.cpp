3b6987ce65c4e9c302955b6e48b10e85c6e213c9
Yinuo Peng <yinuop2@uci.edu>
2024-10-19 00:15:13 +0000

    complete post_process.cpp

--- src/post_process.cpp
+++ src/post_process.cpp
@@ -1,1 +1,30 @@
 // complete the source code
+#include "../include/post_process.h"
+
+bool isNumPrime(int num){
+    if (num <= 1){
+        return false;
+    }
+    if (num == 2){
+        return true;
+    }
+    if (num % 2 == 0){
+        return false;
+    } 
+
+    for (int i = 3; i <= sqrt(num); i += 2) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int addTen(int num){
+    if (num < 10){
+        return num += 10;
+    }else{
+        return num;
+    }
+}
+
